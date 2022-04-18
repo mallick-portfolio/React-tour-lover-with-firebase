b508b0bc7c47d9da77426b608045823c04571312
mallick-portfolio <tamal@gmail.com>
2022-04-18 23:50:35 +0600

    all done

--- src/pages/Shared/Footer/Footer.js
+++ src/pages/Shared/Footer/Footer.js
@@ -1,72 +1,157 @@
 import React from "react";
-
+import { Link, NavLink } from "react-router-dom";
+import "./Footer.css";
 const Footer = () => {
   return (
-    
-      <footer className="text-center text-white">
-        <div className="container pt-4">
-          <section className="mb-4">
-            <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
-              role="button"
-              data-mdb-ripple-color="dark"
-            >
-              <i className="fab fa-facebook-f"></i>
-            </a>
-
-            <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
-              role="button"
-              data-mdb-ripple-color="dark"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-
-            <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
-              role="button"
-              data-mdb-ripple-color="dark"
-            >
-              <i className="fab fa-google"></i>
-            </a>
-
-            <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
-              role="button"
-              data-mdb-ripple-color="dark"
-            >
-              <i className="fab fa-instagram"></i>
-            </a>
-
-            <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
-              role="button"
-              data-mdb-ripple-color="dark"
-            >
-              <i className="fab fa-linkedin"></i>
-            </a>
-            <a
-              className="btn btn-link btn-floating btn-lg text-dark m-1"
-              href="#!"
-              role="button"
-              data-mdb-ripple-color="dark"
-            >
-              <i className="fab fa-github"></i>
-            </a>
-          </section>
+    <footer className="footer-section">
+      <div className="container">
+        <div className="footer-cta pt-5 pb-5">
+          <div className="row">
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="fas fa-map-marker-alt"></i>
+                <div className="cta-text">
+                  <h4>Find us</h4>
+                  <span>1010 Avenue, Dhaka, Bangladesh</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="fas fa-phone"></i>
+                <div className="cta-text">
+                  <h4>Call us</h4>
+                  <span>+880xxxxxxxxxx</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-md-4 mb-30">
+              <div className="single-cta">
+                <i className="far fa-envelope-open"></i>
+                <div className="cta-text">
+                  <h4>Mail us</h4>
+                  <span>tamal1714019@gmail.com</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        <div className="footer-content pt-5 pb-5">
+          <div className="row">
+            <div className="col-xl-4 col-lg-4 mb-50">
+              <div className="footer-widget">
+                <div className="footer-logo">
+                  <h4  className="text-white">
+                    Tour Lover Pro
+                  </h4>
+                </div>
+                <div className="footer-social-icon">
+                  <span>Follow us</span>
+                  <a href="#">
+                    <i className="fab fa-facebook-f facebook-bg"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fab fa-twitter twitter-bg"></i>
+                  </a>
+                  <a href="#">
+                    <i className="fab fa-google-plus-g google-bg"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+              <div className="footer-widget">
+                <div className="footer-widget-heading">
+                  <h3>Useful Links</h3>
+                </div>
+                <ul>
+                  <li>
+                    <Link to={"/"}>Home</Link>
+                  </li>
 
-        <div
-          className="text-center text-dark p-3"
-        >
-          © 2020 Copyright:Tamal Mallick
+                  <li>
+                    <Link to={"/about"}>About</Link>
+                  </li>
+
+                  <li>
+                    <Link to={"/services"}>Services</Link>
+                  </li>
+                  <li>
+                    <Link to={"/blogs"}>Blogs</Link>
+                  </li>
+                  <li>
+                    <Link to={"/"}>Home</Link>
+                  </li>
+
+                  <li>
+                    <Link to={"/about"}>About</Link>
+                  </li>
+
+                  <li>
+                    <Link to={"/services"}>Services</Link>
+                  </li>
+                  <li>
+                    <Link to={"/blogs"}>Blogs</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
+              <div className="footer-widget">
+                <div className="footer-widget-heading">
+                  <h3>Subscribe</h3>
+                </div>
+                <div className="footer-text mb-25">
+                  <p>
+                    Don’t miss to subscribe to our new feeds, kindly fill the
+                    form below.
+                  </p>
+                </div>
+                <div className="subscribe-form">
+                  <form action="#">
+                    <input type="text" placeholder="Email Address" />
+                    <button>
+                      <i className="fab fa-telegram-plane"></i>
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </footer>
+      </div>
+      <div className="copyright-area">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+              <div className="copyright-text">
+                <p>Copyright &copy; 2018, All Right Reserved Tamal Mallick</p>
+              </div>
+            </div>
+            <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+              <div className="footer-menu">
+                <ul>
+                  <li>
+                    <Link to={"/"}>Home</Link>
+                  </li>
+
+                  <li>
+                    <Link to={"/about"}>About</Link>
+                  </li>
+
+                  <li>
+                    <Link to={"/services"}>Services</Link>
+                  </li>
+                  <li>
+                    <Link to={"/blogs"}>Blogs</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
